308b4c6c164d20e53773cd52a51382c83c8cf1e3
Alec Ortega <aleccortega@gmail.com>
2019-02-27 12:01:49 -0500

    Move testing into tests folder

---
+++ src/RecipeForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from './core/Card';
+
+class RecipeForm extends React.Component {
+  render() {
+    return (
+      <Card>
+        <h2>Create a recipe</h2>
+        <form />
+      </Card>
+    );
+  }
+}
+
+export default RecipeForm;
